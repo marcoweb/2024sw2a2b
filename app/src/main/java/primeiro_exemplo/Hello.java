926e77e7f4fc4a9581f673b8a88cbc1dba2dbfbb
marcoweb <marcocarvalho.web@gmail.com>
2024-02-16 12:26:21 +0000

    Classes

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -1,5 +1,7 @@
 package primeiro_exemplo;
 
+import primeiro_exemplo.modelos.Pessoa;
+
 public class Hello {
     public static void main(String[] args) {
         int cont = 10;
@@ -7,5 +9,25 @@ public class Hello {
         System.out.println("Olá Mundo!!!");
 
         System.out.println(cont + " : " + nome);
+
+        Pessoa p1 = new Pessoa();
+
+        p1.nome = "Marco";
+        p1.idade = 43;
+
+        System.out.println(p1.nome + " : " + p1.idade);
+
+        Pessoa p2 = p1;
+        p2.nome = "Isaque";
+
+        System.out.println(p2.nome);
+        System.out.println(p1.nome);
+
+        Pessoa p3 = new Pessoa();
+        p3.nome = "Zezinho";
+        p3.idade = 16;
+
+        System.out.println(p1.verificarMarioridade());
+        System.out.println(p3.verificarMarioridade());
     }
 }
